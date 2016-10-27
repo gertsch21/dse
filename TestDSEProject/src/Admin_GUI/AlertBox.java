e33d5a3161ee25e4543ab9d615bb8f9f92a40a90
Josef Weber <josefweber@eduroam-028-156.wlan.univie.ac.at>
2016-10-27 16:27:05 +0200

    GUI ADMIN YO

---
+++ TestDSEProject/src/Admin_GUI/AlertBox.java
@@ -0,0 +1,37 @@
+package Admin_GUI;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Created by josefweber on 27.10.16.
+ */
+public class AlertBox {
+    public static void display(String title, String message){
+        Stage window = new Stage();
+
+        // block input Events with other Windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+        window.setMinHeight(100);
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Close Window");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label,closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}
---
+++ TestDSEProject/src/Admin_GUI/PhysicalServer.java
@@ -0,0 +1,41 @@
+package Admin_GUI;
+
+import network.server.SimpleServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by josefweber on 27.10.16.
+ */
+public class PhysicalServer extends Thread  {
+
+    private ServerSocket serverSocket;
+
+    public void run(){
+        try {
+            serverSocket = new ServerSocket(1234);
+            System.out.println("Server gestartet(Port " + serverSocket.getLocalPort() + ")");
+            try {
+                while (true) {
+                    System.out.println("Server wartet auf eine neue Verbindung!");
+                    new SimpleServer(serverSocket.accept());
+                }
+            } catch (IOException e) {
+                System.err.println("SimpleServer:main: Connetion Failed");
+                System.exit(1);
+            }
+
+        } catch (IOException e) {
+            System.err.println("SimpleServer:main: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public void shutdown() throws IOException {
+        this.serverSocket.close();
+        System.out.println("Closing Serversocket port: " + serverSocket.getLocalPort());
+
+    }
+
+}
